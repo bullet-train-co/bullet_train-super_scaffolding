40b4d720ea9ff528b81de9e0be244dc784b94cb8
Gabriel Zayas <g-zayas@hotmail.com>
2022-06-02 22:59:36 +0900

    Scaffold routes correctly when namespaces match, and when child does not have the same namespace

--- lib/scaffolding/routes_file_manipulator.rb
+++ lib/scaffolding/routes_file_manipulator.rb
@@ -380,14 +380,14 @@ class Scaffolding::RoutesFileManipulator
       #   resources :deliverables, except: collection_actions
       # end
 
+      # We want to see if there are any namespaces one level above the parent itself,
+      # because namespaces with the same name as the resource can exist on the same level.
+      parent_block_start = find_block_parent(parent_within)
+      namespace_line_within = find_or_create_namespaces(child_namespaces, parent_block_start)
+      find_or_create_resource([child_resource], options: "except: collection_actions", within: namespace_line_within)
       unless find_namespaces(child_namespaces, within)[child_namespaces.last]
-        insert_after(["", "namespace :#{child_namespaces.last} do", "end"], find_block_end(scope_within))
-        unless find_namespaces(child_namespaces, within)[child_namespaces.last]
-          raise "tried to insert `namespace :#{child_namespaces.last}` but it seems we failed"
-        end
+        raise "tried to insert `namespace :#{child_namespaces.last}` but it seems we failed"
       end
-
-      find_or_create_resource(child_namespaces + [child_resource], options: "except: collection_actions", within: within)
 
     # e.g. Projects::Deliverable and Objective Under It, Abstract::Concept and Concrete::Thing
     elsif parent_namespaces.any?
@@ -395,18 +395,16 @@ class Scaffolding::RoutesFileManipulator
       # namespace :projects do
       #   resources :deliverables
       # end
-      #
+      top_parent_namespace = find_namespaces(parent_namespaces, within)[parent_namespaces.first]
+      find_or_create_resource(child_namespaces + [child_resource], within: top_parent_namespace)
+
       # resources :projects_deliverables, path: 'projects/deliverables' do
       #   resources :objectives
       # end
-
-      find_resource(parent_namespaces + [parent_resource], within: within)
-      top_parent_namespace = find_namespaces(parent_namespaces, within)[parent_namespaces.first]
       block_parent_within = find_block_parent(top_parent_namespace)
       parent_namespaces_and_resource = (parent_namespaces + [parent_resource]).join("_")
       parent_within = find_or_create_resource_block([parent_namespaces_and_resource], options: "path: '#{parent_namespaces_and_resource.tr("_", "/")}'", within: block_parent_within)
       find_or_create_resource(child_namespaces + [child_resource], within: parent_within)
-
     else
 
       begin
